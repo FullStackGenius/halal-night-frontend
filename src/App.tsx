8d127c6042c843c6e85150fddc035cecd1d236e6
FullStackGenius <upliftwebdev@gmail.com>
2025-11-11 11:51:47 +0530

    change in path

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,10 @@ import Home from './pages/Home'
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Step from './pages/Step';
 import Prayers from './pages/Prayers';
-// import Contract from './pages/Contract';
+import Contract from './pages/Contract';
 import Faq from './pages/Faq';
 
-// import ContactUs from './pages/ContactUs';
+import ContactUs from './pages/ContactUs';
 function App() {
 
   return (
@@ -18,10 +18,10 @@ function App() {
           <Route path="/about" element={<About />} />
           <Route path="/steps" element={<Step />} />
           <Route path="/prayers" element={<Prayers />} />
-          {/* <Route path="/contract" element={<Contract />} /> */}
+          <Route path="/test-work" element={<Contract />} />
            <Route path="/contract" element={<Home />} />
           <Route path="/faq" element={<Faq />} />
-          {/* <Route path="/contact" element={<ContactUs />} /> */}
+          <Route path="/test-contact" element={<ContactUs />} />
            <Route path="/contact" element={<Home />} />
         </Routes>
       </Router>
